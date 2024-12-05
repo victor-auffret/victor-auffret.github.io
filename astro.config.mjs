27460fd82a267b7103ce6a4736bd4c62fb74ec13
victor-auffret <victor.auffret@orange.fr>
2024-12-05 12:24:03 +0100

    config solidjs git pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,27 @@
 import { defineConfig } from "astro/config";
 import solid from "@astrojs/solid-js";
+//import sitemap from "@astrojs/sitemap";
+import solid from "@astrojs/solid-js";
+//import mdx from "@astrojs/mdx";
+/*
+import Compress from "astro-compress";
+Compress({
+  CSS: false,
+    Image: false,
+    Action: {
+      Passed: async () => true, // https://github.com/PlayForm/Compress/issues/376
+    },
+  }),
+*/
 
 export default defineConfig({
-  integrations: [solid()],
+  //prefetch: true,
+  site: "https://victor-auffret.github.io/",
+  base: "/",
+  integrations: [
+    solid({
+      include: ["**/solid/*"],
+    })
+  ],
   outDir: "docs",
 });
