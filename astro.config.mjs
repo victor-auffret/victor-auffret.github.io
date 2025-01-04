a0a0231de51771bfad942832f67d3375a89665db
victor-auffret <victor.auffret@orange.fr>
2025-01-04 15:32:44 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,14 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import Compress from "astro-compress";
 
+const DEV = process.env?.NODE_ENV == "development"
+const PROD = !DEV && process.env?.NODE_ENV ===  "production"
+
 export default defineConfig({
   prefetch: true,
   site: "https://victor-auffret.github.io/",
   //base: "/",
-  base: '/docs/',
+  base: PROD ? '/docs/' : "/",
   i18n: {
     defaultLocale: "fr",
     locales: ["fr"],
---
+++ src/content/config.astuces.tsx
@@ -0,0 +1,29 @@
+import { defineCollection, z } from 'astro:content'
+import { glob } from 'astro/loaders'
+
+const astuces = defineCollection({
+  loader: glob({ base: './src/content/astuces', pattern: '**/*.{md,mdx}' }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string().max(60),
+      description: z.string().max(160),
+      publishDate: z.coerce.date(),
+      /*
+      // Optional
+      updatedDate: z.coerce.date().optional(),
+      heroImage: z
+        .object({
+          src: image(),
+          alt: z.string().optional(),
+          color: z.string().optional()
+        })
+        .optional(),
+      language: z.string().optional(),
+      draft: z.boolean().default(false),
+      // Integrations
+      comment: z.boolean().default(true)
+      */
+    })
+})
+
+export const collections = { astuces }
